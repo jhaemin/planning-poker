cd0c5e4d9356e2854bc43a86cf3e7468933ccc7d
Jang Haemin <io@jhaemin.com>
2024-07-26 17:28:59 +0900

    Update meta

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -11,8 +11,16 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Planning Poker</title>
-        <meta name="og:title" content="Planning Poker" />
-        <meta name="og:image" content="https://pp.land/og/og-image.png" />
+        <meta
+          name="description"
+          content="The world's simplest Planning Poker."
+        />
+        <meta property="og:title" content="Planning Poker" />
+        <meta
+          property="og:description"
+          content="The world's simplest Planning Poker."
+        />
+        <meta property="og:image" content="https://pp.land/og/og-image.png" />
       </Head>
       <Provider store={globalStore}>
         <PlanningPokerLayout>
